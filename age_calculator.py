767adf6c7c280c97595c5a16e06ae126edc1d4aa
ShahdAllemie0 <69586588+ShahdAllemie0@users.noreply.github.com>
2020-08-17 11:26:27 +0300

    Update age_calculator.py

--- age_calculator.py
+++ age_calculator.py
@@ -1,42 +1,40 @@
-# from datetime import datetime
+from datetime import datetime
+from datetime import date
+def check_birthdate(year, month, day):
+  
+	     # write code here
+       
+          birthdate = date(int(year), int(month),int(day) )
+          today = date.today()
+          if birthdate<today:
+           return True
+          else:
+           return False
 
-# def check_birthdate(year, month, day):
-# 	# write code here
-
-# def calculate_age(year, month, day):
-#     # write code here
-
-# def main():
-# 	# write main code here
-
-
-# if __name__ == '__main__':
-# 	main()
-def main():
-    
-   year=input("Enter year of birth:")
-   month=input("Enter month of birth:")
-   day=input("Enter day of birth:")
-   from datetime import date  
-   def check_birthdate(year,month,day):
-      birthdate = date(int(year), int(month),int(day) )
-      today = date.today()
-      if birthdate<today:
-        calculate_age(int(year), int(month),int(day))
-        return True
-      else:
-        print("the birthdate is invalid" )
-        return False
-        
-        # return "the birthdate is invalid"  
-   def calculate_age(year,month,day):
+def calculate_age(year, month, day):
           birthdate = date(int(year), int(month),int(day) )
           today = date.today()
           age=today - birthdate
           agedays=age.days
           yourage=int(agedays/365)
-          print("You are %d years old."%(yourage))  
-   check_birthdate(year,month,day)   
+          print("You are %d years old."%(yourage)) 
+
+def main():
+	# write main code here
+	    
+   year=input("Enter year of birth: ")
+   month=input("Enter month of birth: ")
+   day=input("Enter day of birth: ")
+
+ 
+   if(check_birthdate(year,month,day))==True:
+     calculate_age(int(year), int(month),int(day))
+   else:
+     print("the birthdate is invalid" )
+
 
 if __name__ == '__main__':
 	main()
+
+
+
